9b00102333a84cdea5ad085bae69b454856508f8
Ignacio Sanchez Gines <isanchez@redhat.com>
2019-05-29 17:08:43 +0200

    test

--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
         "fmt"
 )
 
+var memory []byte
+
 func main() {
-        mem := make([]byte, 1024*1024*1024)
+        memory = make([]byte, 1024*1024*1024)
 	http.HandleFunc("/", helloWorldHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+        memory[0] = 0
 	name, _ := os.Hostname()
 	io.WriteString(w, "Hello world from host \"" + name + "\"\n")
 }
