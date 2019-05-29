a970a6e20c333c019638299eaee019959493e857
Ignacio Sanchez Gines <isanchez@redhat.com>
2019-05-29 17:34:00 +0200

    test

--- main.go
+++ main.go
@@ -9,16 +9,17 @@ import (
 var buff []byte
 
 func main() {
-        buff = make([]byte, 1024*1024*1024, 1024*1024*1024)
+        size := 1024 * 1024 * 1024
+        buff = make([]byte, size)
+	for i := 0; i < size; i += 1 {
+            buff[i] = byte(i)
+        }
 	http.HandleFunc("/", helloWorldHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-        size := 1024 * 1024 * 1024
-	for i := 0; i < size; i += 1 {
-            buff[i] = byte(i)
-        }
+	buff[0] = 0
 	name, _ := os.Hostname()
 	io.WriteString(w, "Hello world from host \"" + name + "\"\n")
 }
